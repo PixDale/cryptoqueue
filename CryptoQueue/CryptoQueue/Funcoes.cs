29da1e17013c955b578aace594358f85ec3c62a7
felipe <felipegaldino16@gmail.com>
2018-11-29 00:54:04 -0200

    Adicionado Classe abstrata de funcoes, com esqueleto das principais funções

---
+++ CryptoQueue/CryptoQueue/Funcoes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoQueue
+{
+    abstract class Funcoes
+    {
+
+        public int fib(int n)
+        {
+            // Implementar fibonacci
+            return 0;
+        }
+        public int ConverteDecBin()
+        {
+            //Implementar conversor de decimal para binario
+            return 0;
+        }
+        public int ConverteBinDec()
+        {
+            //Implementar conversor de binario para decimal
+            return 0;
+        }
+        public int ConverteBinFib()
+        {
+            //Implementar conversor de binario para formato fibonacci
+            return 0;
+        }
+        public int ConverteFibBin()
+        {
+            //Implementar conversor de formato fibonacci para binario
+            return 0;
+        }
+    }
+}
